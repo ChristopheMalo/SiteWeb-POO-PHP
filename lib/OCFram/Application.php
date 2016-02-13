e9ab4f09b83110453ce901d2dafa660d4e58f2ba
ChristopheMalo <christof.malo@gmail.com>
2016-02-13 16:40:42 +0100

    Concevoir et coder la classe Application

---
+++ lib/OCFram/Application.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OCFram;
+
+/**
+ * Classe représentant l'Application
+ * TP Créer un site web - POO en PHP
+ * 
+ * @author      Christophe Malo
+ * @date        13/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+abstract class Application {
+    protected $httpRequest,
+              $httpResponse,
+              $name;
+    
+    public function __construct() {
+        $this->httpRequest  = new HTTPRequest;
+        $this->httpResponse = new HTTPResponse;
+        $this->name         = '';
+    }
+    
+    abstract public function run();
+    
+    public function httpRequest() {
+        return $this->httpRequest;
+    }
+    
+    public function httpResponse() {
+        return $this->httpResponse;
+    }
+    
+    public function name() {
+        return $this->name;
+    }
+}
