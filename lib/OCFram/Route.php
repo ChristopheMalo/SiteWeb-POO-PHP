1d9284d37911beda55397aae001b36888fa11977
ChristopheMalo <christof.malo@gmail.com>
2016-02-14 12:00:05 +0100

    Concevoir et coder les classes Routeur et Route

---
+++ lib/OCFram/Route.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace OCFram;
+
+/**
+ * Classe permettant de renvoyer une route correspondand à une URL 
+ * TP Créer un site web - POO en PHP
+ * 
+ * @author      Christophe Malo
+ * @date        14/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+class Route {
+
+    protected $action;
+    protected $module;
+    protected $url;
+    protected $varsNames;
+    protected $vars = [];
+
+    public function __construct($url, $module, $action, array $varsNames) {
+        $this->setUrl($url);
+        $this->setModule($module);
+        $this->setAction($action);
+        $this->setVarsNames($varsNames);
+    }
+
+    public function hasVars() {
+        return !empty($this->varsNames);
+    }
+
+    public function match($url) {
+        if (preg_match('`^' . $this->url . '$`', $url, $matches)) {
+            return $matches;
+        } else {
+            return false;
+        }
+    }
+    
+    /* Setters */
+    public function setAction($action) {
+        if (is_string($action)) {
+            $this->action = $action;
+        }
+    }
+
+    public function setModule($module) {
+        if (is_string($module)) {
+            $this->module = $module;
+        }
+    }
+
+    public function setUrl($url) {
+        if (is_string($url)) {
+            $this->url = $url;
+        }
+    }
+
+    public function setVarsNames(array $varsNames) {
+        $this->varsNames = $varsNames;
+    }
+
+    public function setVars(array $vars) {
+        $this->vars = $vars;
+    }
+
+    /* Getters */
+    public function action() {
+        return $this->action;
+    }
+
+    public function module() {
+        return $this->module;
+    }
+    
+    public function url() {
+        return $this->url;
+    }
+
+    public function vars() {
+        return $this->vars;
+    }
+
+    public function varsNames() {
+        return $this->varsNames;
+    }
+
+}
