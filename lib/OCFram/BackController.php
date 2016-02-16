fa4e8995f0d49cc7a38650d376bbd4cffd9dbb40
ChristopheMalo <christof.malo@gmail.com>
2016-02-17 00:31:44 +0100

    Concevoir et coder la class Page + updater BackController

--- lib/OCFram/BackController.php
+++ lib/OCFram/BackController.php
@@ -68,6 +68,8 @@ abstract class BackController extends ApplicationComponent {
         }
 
         $this->view = $view;
+        
+        $this->page->setContentFile(__DIR__.'/../../App/'.$this->app->name().'/Modules/'.$this->module.'/Views/'.$this->view.'.php');
     }
 
 }
---
+++ lib/OCFram/Page.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OCFram;
+
+/**
+ * Classe permettant de générer une page
+ * composée d'une vue et d'un layout
+ * Une page peut recevoir une variable
+ * TP Créer un système de news - POO en PHP
+ *
+ * @author      Christophe Malo
+ * @date        13/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+class Page extends ApplicationComponent {
+    protected $contentFile,
+              $vars = [];
+    
+    public function addVar($var, $value) {
+        if (!is_string($var) || is_numeric($var) || empty($var)) {
+            throw new \InvalidArgumentException('Le nom de la variable doit être une chaine de caractères non nulle');
+        }
+
+        $this->vars[$var] = $value;
+    }
+    
+    public function getGeneratedPage() {
+        if (!file_exists($this->contentFile)) {
+            throw new \RuntimeException('La vue spécifiée n\'existe pas');
+        }
+
+        extract($this->vars);
+
+        ob_start();
+        require $this->contentFile;
+        $content = ob_get_clean();
+
+        ob_start();
+        require __DIR__ . '/../../App/' . $this->app->name() . '/Templates/layout.php';
+        return ob_get_clean();
+    }
+    
+    public function setContentFile() {
+        if (!is_string($contentFile) || empty($contentFile)) {
+            throw new \InvalidArgumentException('La vue spécifiée est invalide');
+        }
+
+        $this->contentFile = $contentFile;
+    }
+}
