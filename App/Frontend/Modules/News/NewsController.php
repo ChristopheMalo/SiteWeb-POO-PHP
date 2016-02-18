07dee23bd49968e57546d043aa1507b8578594d7
ChristopheMalo <christof.malo@gmail.com>
2016-02-18 20:08:08 +0100

    Coder et concevoir l'action index - route et controleur module News

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -1,8 +1,47 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace App\Frontend\Modules\News;
 
+use \OCFram\BackController;
+use \OCFram\HTTPRequest;
+
+/**
+ * Controleur du module News
+ * TP Créer un site web - POO en PHP
+ *
+ * @author      Christophe Malo
+ * @date        18/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+class NewsController extends BackController {
+    /**
+     * Méthode pour l'action index
+     * 
+     * @param HTTPRequest $request
+     */
+    public function executeIndex(HTTPRequest $request) {
+        $nombreNews = $this->app->config()->get('nombre_news');
+        $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+
+        // Ajouter une définition pour le titre.
+        $this->page->addVar('title', 'Liste des ' . $nombreNews . ' dernières news');
+
+        // Récupérer le manager des news.
+        $manager = $this->managers->getManagerOf('News');
+
+        $listeNews = $manager->getList(0, $nombreNews);
+
+        foreach ($listeNews as $news) {
+            if (strlen($news->contenu()) > $nombreCaracteres) {
+                $debut = substr($news->contenu(), 0, $nombreCaracteres);
+                $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+
+                $news->setContenu($debut);
+            }
+        }
+
+        // Ajouter la variable $listeNews à la vue.
+        $this->page->addVar('listeNews', $listeNews);
+    }
+}
