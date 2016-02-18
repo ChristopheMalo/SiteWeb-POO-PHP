038cc0586411f8f488597143f8c372c6ad1b5463
ChristopheMalo <christof.malo@gmail.com>
2016-02-18 21:39:11 +0100

    Coder les 2 calss du modèle pour l'action index du module News

--- lib/vendors/Model/NewsManager.php
+++ lib/vendors/Model/NewsManager.php
@@ -1,8 +1,27 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Model;
 
+use \OCFram\Manager;
+/**
+ * Classe premettant de gérer les news
+ * Cette classe doit intéragir avec la DB
+ * Elle n'est pas dépendante de PDO
+ * D'où la création d'une classes spécialisée pour PDO
+ * La classe spécialisée va hériter de NewsManager
+ * TP Créer un site web - POO en PHP
+ * 
+ * @author      Christophe Malo
+ * @date        18/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+abstract class NewsManager extends Manager {
+    /**
+     * Méthode retournant une liste de news demandée.
+     * @param $debut int La première news à sélectionner
+     * @param $limite int Le nombre de news à sélectionner
+     * @return array La liste des news. Chaque entrée est une instance de News.
+     */
+    abstract public function getList($debut = -1, $limite = -1);
+}
--- lib/vendors/Model/NewsManagerPDO.php
+++ lib/vendors/Model/NewsManagerPDO.php
@@ -1,8 +1,40 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Model;
 
+use \Entity\News;
+
+/**
+ * Classe spécialisée pour se connecter à la DB en PDO
+ * Cette classe hérite de NewsManager
+ * TP Créer un site web - POO en PHP
+ * 
+ * @author      Christophe Malo
+ * @date        18/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+class NewsManagerPDO extends NewsManager {
+    public function getList($debut = -1, $limite = -1) {
+        $sql = 'SELECT id, auteur, titre, contenu, dateAjout, dateModif FROM news ORDER BY id DESC';
+
+        if ($debut != -1 || $limite != -1) {
+            $sql .= ' LIMIT ' . (int) $limite . ' OFFSET ' . (int) $debut;
+        }
+
+        $requete = $this->dao->query($sql);
+        $requete->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\News');
+
+        $listeNews = $requete->fetchAll();
+
+        foreach ($listeNews as $news) {
+            $news->setDateAjout(new \DateTime($news->dateAjout()));
+            $news->setDateModif(new \DateTime($news->dateModif()));
+        }
+
+        $requete->closeCursor();
+
+        return $listeNews;
+    }
+
+}
