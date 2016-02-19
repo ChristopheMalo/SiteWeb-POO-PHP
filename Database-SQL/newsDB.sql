a0a2e3046fb180cf541daaa1643d70c5a9b58d3f
ChristopheMalo <christof.malo@gmail.com>
2016-02-19 19:25:00 +0100

    Mettre à jour le SQL Dump avec la table comments

--- Database-SQL/newsDB.sql
+++ Database-SQL/newsDB.sql
@@ -3,22 +3,30 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Lun 08 Février 2016 à 19:54
+-- Généré le :  Ven 19 Février 2016 à 19:22
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de données :  `newsDB`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` mediumint(9) NOT NULL,
+  `news` smallint(6) NOT NULL,
+  `auteur` varchar(50) NOT NULL,
+  `contenu` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -40,6 +48,12 @@ CREATE TABLE `news` (
 --
 
 --
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `news`
 --
 ALTER TABLE `news`
@@ -50,10 +64,12 @@ ALTER TABLE `news`
 --
 
 --
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `news`
 --
 ALTER TABLE `news`
   MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
