f881b833cc10683d549b9295d72f15e65a635ad6
ChristopheMalo <christof.malo@gmail.com>
2016-02-20 11:11:36 +0100

    Coder affichage des commentaires dans la vue show de la news

--- App/Frontend/Modules/News/Views/show.php
+++ App/Frontend/Modules/News/Views/show.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 
- * Vue pour l'action show (executeShow du module News
+ * Vue pour l'action show (executeShow du module News)
  * TP Créer un site web - POO en PHP
  *
  * @author      Christophe Malo
@@ -10,11 +10,35 @@
  * @copyright   OpenClassrooms - Victor Thuillier
  */
 ?>
+<!-- Affichage de la news -->
 <p>Par <em><?= $news['auteur'] ?></em>, le <?= $news['dateAjout']->format('d/m/Y à H\hi') ?></p>
 <h2><?= $news['titre'] ?></h2>
 <p><?= nl2br($news['contenu']) ?></p>
 
 <?php if ($news['dateAjout'] != $news['dateModif']) { ?>
+    
     <p style="text-align: right;"><small><em>Modifiée le <?= $news['dateModif']->format('d/m/Y à H\hi') ?></em></small></p>
-<?php
-}
+
+<?php } ?>
+
+<!-- Gestion des commentaires -->    
+<p><a href="commenter-<?= $news['id'] ?>.html">Ajouter un commentaire</a></p>
+
+<?php if (empty($comments)) { ?>
+    
+    <p>Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !</p>
+
+<?php }
+
+foreach ($comments as $comment) { ?>
+    
+    <fieldset>
+        <legend>
+            Posté par <strong><?= htmlspecialchars($comment['auteur']) ?></strong> le <?= $comment['date']->format('d/m/Y à H\hi') ?>
+        </legend>
+        <p><?= nl2br(htmlspecialchars($comment['contenu'])) ?></p>
+    </fieldset>
+
+<?php } ?>
+
+<p><a href="commenter-<?= $news['id'] ?>.html">Ajouter un commentaire</a></p>
