3bc95974e0228424f5f3887cbe665d29d4508658
ChristopheMalo <christof.malo@gmail.com>
2016-02-22 23:27:46 +0100

    Ajouter des commentaires à la class ConnexionController

--- App/Backend/Modules/Connexion/ConnexionController.php
+++ App/Backend/Modules/Connexion/ConnexionController.php
@@ -23,9 +23,12 @@ class ConnexionController extends BackController
      * Cette méthode , si le formulaire est envoyé, vérifie
      *  - le login
      *  - le password
-     * Si les informations siasies sont correctes alors
-     * l'utilisateur est authentifié
+     * Si les informations saisies sont correctes alors
+     * l'utilisateur du backend est authentifié
      * Sinon est message d'erreur est affiché
+     * 
+     * Le login et le mot de passe sont renseignés dans le fichier de config app.xml
+     * Évolution possible : Un système évolué en DB avec plusieurs users
      * 
      * @param HTTPRequest $request
      */
