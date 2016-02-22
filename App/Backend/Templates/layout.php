a854cf93520e049bc9f9c957f877577b1291ff50
ChristopheMalo <christof.malo@gmail.com>
2016-02-22 21:54:11 +0100

    Coder le layout du Backend

---
+++ App/Backend/Templates/layout.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Pour simplifier le développement, le layout du backend est identique à celui du frontend
+ * En général, les layouts sont différents.
+ * 
+ * Pour éviter de copier coller le code, utilisation de require
+ */
+
+require __DIR__.'/../../Frontend/Templates/layout.php';
