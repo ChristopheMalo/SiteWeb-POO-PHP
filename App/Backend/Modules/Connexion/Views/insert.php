d950e600b1ac970338d44e7a04495bdd9c7b25b5
ChristopheMalo <christof.malo@gmail.com>
2016-02-23 21:57:52 +0100

    Coder la vue news-insert Backend
    Cette vue comprend un formulaire dans un fichier à inclure

---
+++ App/Backend/Modules/Connexion/Views/insert.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Vue pour l'action d'insertion d'une news news-insert du module News Backend
+ * Insert et Update utilisent cett view
+ * Mise en place de l'inclusion d'un fichier form d'ou le nom _form.php
+ * 
+ * TP Créer un site web - POO en PHP
+ *
+ * @author      Christophe Malo
+ * @date        22/02/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+?>
+<h2>Ajouter une news</h2>
+<?php require '_form.php';
