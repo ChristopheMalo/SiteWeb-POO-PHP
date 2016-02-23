c164ae39d36341cf59dd2007bfff42dc6204b943
ChristopheMalo <christof.malo@gmail.com>
2016-02-23 21:47:37 +0100

    Coder methodes save() et add() Model News pour Backend

--- lib/vendors/Model/NewsManagerPDO.php
+++ lib/vendors/Model/NewsManagerPDO.php
@@ -66,5 +66,16 @@ class NewsManagerPDO extends NewsManager
     {
         return $this->dao->query('SELECT COUNT(*) FROM news')->fetchColumn();
     }
+    
+    protected function add(News $news)
+    {
+        $requete = $this->dao->prepare('INSERT INTO news SET auteur = :auteur, titre = :titre, contenu = :contenu, dateAjout = NOW(), dateModif = NOW()');
+    
+        $requete->bindValue(':titre',   $news->titre());
+        $requete->bindValue(':auteur',  $news->auteur());
+        $requete->bindValue(':contenu', $news->contenu());
+
+        $requete->execute();
+    }
 
 }
